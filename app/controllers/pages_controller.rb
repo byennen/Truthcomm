a0c345183119af0f4db418eeb08d6ac1cfbffd19
Lance Ennen <byennen@gmail.com>
2011-05-08 11:31:36 -0500

    frontend design updates and pages added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,38 @@
 class PagesController < ApplicationController
   def index
+    @title = "Welcome"
+  end
+  
+  def services
+    @title = "Services"
   end
 
   def quote
-  end
-
-  def carriers
+    @title = "Quote"
   end
 
   def about
+    @title = "About"
+  end
+  
+  def carriers
+    @title = "Carriers"
+  end
+  
+  def contact
+    @title = "Contact Us"
+  end
+  
+  def energy
+    @title = "Energy Services"
+  end
+  
+  def phone
+    @title = "Phone Hardware"
+  end
+  
+  def broker
+    @title = "Why use a Broker?"
   end
 
 end
