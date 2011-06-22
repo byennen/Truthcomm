965d00bbff6e95e686052f05738885f29f26625e
Lance Ennen <byennen@gmail.com>
2011-06-22 00:24:43 -0500

    added validation and a select drop down to the quotes form

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,7 +2,16 @@ class Quote
   
   include ActiveModel::Validations
 
-  validates_presence_of :name, :email, :phone, :message
+  validates_presence_of :name, :message
+  
+  validates_format_of :phone,
+    :message => "must be a valid telephone number.",
+    :with => /^[\(\)0-9\- \+\.]{10,20}$/
+  
+  validates_format_of :email,
+    :message => 'email must be valid',
+    :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/
+                              
 
   attr_accessor :id, :name, :company_name, :email, :phone, :address, :phone_lines, :current_internet, :current_carrier, :message
 
